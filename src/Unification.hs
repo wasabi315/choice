088e6c6b25d259768a340892bdc1603e0d6a8dcc
Nathaniel Burke <nathanielrburke3@gmail.com>
2026-09-17 22:51:07 +0200

    Style

--- src/Unification.hs
+++ src/Unification.hs
@@ -146,16 +146,14 @@ unify l t u = case (force t, force u) of
   (t, VFlex m' sp') -> trySolve l m' sp' t
   -- TODO: Could probably do something smarter here...
   -- (VChoice c tl tr, VChoice c' tl' tr') = error "TODO"
-  (VChoice _ tl tr, t')
+  (VChoice c tl tr, t')
     | antiUnifies l tl t' && antiUnifies l tr t'
     -> mismatch
-  (VChoice c tl tr, t')
     | antiUnifies l tl t'
     -> tryRefine c R >> unify l tr t'
-  (VChoice c tl tr, t')
     | antiUnifies l tr t'
     -> tryRefine c L >> unify l tl t'
-  (VChoice {}, _)
+    | otherwise
     -> stuck -- We cannot make progress (ideally would postpone)
   (t, VChoice c tl' tr') -> unify l (VChoice c tl' tr') t
   _ -> mismatch
